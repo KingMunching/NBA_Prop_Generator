8a4e97fbb143e3b59e8ae63691a6b3df59e2d8e5
KingMunching <Kinglardizabal02@gmail.com>
2024-12-25 02:35:03 -0500

    started

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+# Nikola Jokić
+career = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# pandas data frames (optional: pip install pandas)
+career.get_data_frames()[0]
+
+# json
+career.get_json()
+
+# dictionary
+career.get_dict()
