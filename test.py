6e4a88cf0e562f7055bd26c41fafbdeab82f3c87
KingMunching <Kinglardizabal02@gmail.com>
2024-12-28 19:08:46 -0500

    aaa

--- test.py
+++ test.py
@@ -8,6 +8,9 @@
 # can view end points here
 # https://github.com/swar/nba_api/tree/master/docs/nba_api/stats/endpoints
 
+#  View the outputs of end points
+# https://github.com/swar/nba_api/tree/master/docs/nba_api/stats/endpoints_output
+
 
 # imports
 from nba_api.live.nba.endpoints import scoreboard
@@ -24,30 +27,6 @@ def getRequest():
     team_info = commonteamroster.CommonTeamRoster(team_id=1610612752, season="2024-25")
 
     print(team_info.get_json())
-
-
-def get_stats():
-    # Function to fetch all game logs and filter for Christmas Day games
-    def get_christmas_stats(player_id, season):
-        # Fetch game logs for the player for the specified season
-        player_logs = playergamelog.PlayerGameLog(player_id=player_id, season=season)
-        player_logs_df = player_logs.get_data_frames()[0]
-
-        # Filter for games played on Christmas (December 25)
-        player_logs_df['GAME_DATE'] = pd.to_datetime(player_logs_df['GAME_DATE'])
-        christmas_stats = player_logs_df[player_logs_df['GAME_DATE'].dt.month == 12]
-        christmas_stats = christmas_stats[christmas_stats['GAME_DATE'].dt.day == 25]
-
-        return christmas_stats
-
-    # Stephen Curry's Player ID and example usage
-    player_id = 201939  # Stephen Curry's Player ID
-    season = "2024-25"  # Adjust for the current season or any other season
-    curry_christmas_stats = get_christmas_stats(player_id, season)
-
-    # Display the filtered DataFrame
-    print(curry_christmas_stats['PTS'])
-    print(curry_christmas_stats['AST'])
 
 
 def get_team_players():
