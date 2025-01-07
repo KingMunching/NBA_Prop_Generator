611a54452b42b8b88d9a454e71f0f9d34453b8d6
KingMunching <Kinglardizabal02@gmail.com>
2025-01-07 09:04:38 -0500

    its better this time

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 from nba_api.live.nba.endpoints import scoreboard
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import teams
 from classes.games import Game
+from classes.teams import Team
 
 class controller:
     #list of game objects
@@ -8,22 +10,28 @@ class controller:
 
     def __init__(self):
         pass
+
+    def get_team_id(self, t):
+        return teams.find_teams_by_full_name(f'{t}')[0].get("id")
     
     # Get today's games
-    def load_games():
+    def load_games(self):
         today_games_dict = scoreboard.ScoreBoard().__dict__
         games_today = []
         for games in today_games_dict:
-            #create a new game object
-            game = Game(games['homeTeam'], games['awayTeam'])
+                                        # New York               +      Knicks                      
+            team1Name, team2Name = games["homeTeam"]["teamCity"] +" "+ games["homeTeam"]["teamName"], games["awayTeam"]["teamCity"]+" "+games["awayTeam"]["teamName"]
+            team1ID, team2ID = self.get_team_id(team1Name), self.get_team_id(team2Name)
+            game = Game(Team(team1ID,team1Name), Team(team2ID,team2Name))
             games_today.append(game)
         return games_today
     
-    def load_team_data():
-        return None
+    def load_players_in_team(self, team:Team):
+            team.set_players()
+            pass
     
     def load_player_data(self, player_id):
         return None
-
+    
 
        
