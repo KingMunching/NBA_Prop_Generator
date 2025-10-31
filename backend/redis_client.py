53a67a2189954485a59748e11bc3a88f20fb5599
KingMunching <kinglardizabal.02@gmail.com>
2025-10-31 17:12:05 -0400

    fixed redis to fetch from server.

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -3,8 +3,11 @@ import os
 
 REDIS_HOST = os.getenv("REDIS_HOST", "LocalHost") 
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,db=0, decode_responses=True)
+print("Redis client configured.")
+
 
 def get_redis_client():
     return redis_client
